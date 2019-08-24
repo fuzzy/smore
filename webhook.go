38b3d2b59e1321f76f61064f27c92d79f93704f8
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2019-08-24 16:34:08 -0700

    tests

--- webhook.go
+++ webhook.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-
-	"gopkg.in/go-playground/webhooks.v5/github"
 )
 
 func GitWebHook(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("%+v\n", r)
+	fmt.Printf("%+v\n", r.Body)
 	switch r.Method {
 	case "POST":
 		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
@@ -23,31 +20,38 @@ func GitWebHook(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func StartWebHook(secret string) {
-	log.Println("Enabling webhook endpoint.")
-	hook, err := github.New(github.Options.Secret(secret))
-	check(err)
-
-	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("webhook payload triggered")
-		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
-		if err != nil {
-			if err != github.ErrEventNotFound {
-				log.Fatal(err)
-			}
-		}
-
-		switch payload.(type) {
-
-		case github.ReleasePayload:
-			release := payload.(github.ReleasePayload)
-			// Do whatever you want from here...
-			fmt.Printf("%+v\n", release)
-		default:
-			release := payload.(github.ReleasePayload)
-			fmt.Printf("%+v\n", release)
-		}
-
-	})
-	http.ListenAndServe(":3000", nil)
-}
+// &{
+// 	Method:POST
+// URL: /webhook
+// 	Proto:HTTP/1.1
+// 	ProtoMajor:1
+// 	ProtoMinor:1
+// 	Header:map[Accept-Encoding:[gzip]
+// 		Content-Length:[3163]
+// 		Content-Type:[application/json]
+// 		User-Agent:[Go-http-client/1.1]
+// 		X-Gitea-Delivery:[460ed7e6-352d-440c-815a-6fcebe519a1d]
+// 		X-Gitea-Event:[push]
+// 		X-Gitea-Signature:[bc5fce2c947a37217fc30bed8a448e9464f77800a8824db15e7c50f4c4635d5b]
+// 		X-Github-Delivery:[460ed7e6-352d-440c-815a-6fcebe519a1d]
+// 		X-Github-Event:[push]
+// 		X-Gogs-Delivery:[460ed7e6-352d-440c-815a-6fcebe519a1d]
+// 		X-Gogs-Event:[push]
+// 		X-Gogs-Signature:[bc5fce2c947a37217fc30bed8a448e9464f77800a8824db15e7c50f4c4635d5b]]
+// 	Body:0xc00044c280
+// 	GetBody:<nil>
+// 		ContentLength:3163
+// 	TransferEncoding:[]
+// 	Close:false
+// 	Host:george-devfu-net:8080
+// 	Form:map[]
+// 	PostForm:map[]
+// 	MultipartForm:<nil>
+// 		Trailer:map[]
+// 	RemoteAddr:10.0.0.156:39836
+// RequestURI:/webhook
+// 	TLS:<nil>
+// 		Cancel:<nil>
+// 		Response:<nil>
+// 		ctx:0xc00044c2c0
+// }
