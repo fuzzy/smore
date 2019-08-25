8801c7fa13cf4049e105307e0c8e18bf1ca11b63
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2019-08-24 17:40:40 -0700

    get rid of old secret code

--- webhook.go
+++ webhook.go
@@ -12,9 +12,6 @@ import (
 	"strings"
 )
 
-// Replace with your hook's secret
-const secret = "shhhhh!!"
-
 func signBody(secret, body []byte) []byte {
 	computed := hmac.New(sha1.New, secret)
 	computed.Write(body)
@@ -33,7 +30,7 @@ func verifySignature(secret []byte, signature string, body []byte) bool {
 	actual := make([]byte, 20)
 	hex.Decode(actual, []byte(signature[5:]))
 
-	return hmac.Equal(signBody(secret, body), actual)
+	return hmac.Equal(signBody(cfg.Git.Webhook.Secret, body), actual)
 }
 
 type HookContext struct {
