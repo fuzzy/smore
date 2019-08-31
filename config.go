fb13f6890113ec5dbb0e2d8ff40292ea3dc21002
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2019-08-31 13:13:32 -0700

    environment configuration

--- config.go
+++ config.go
@@ -12,7 +12,7 @@ import (
 
 type Config struct {
 	Interface string `yaml:"interface"`
-	Port      int    `yaml:"port"`
+	Port      int64  `yaml:"port"`
 	Template  string `yaml:"template"`
 	Dirs      struct {
 		Root      string
@@ -23,12 +23,66 @@ type Config struct {
 	} `yaml:"dirs"`
 	Git struct {
 		Repo     string `yaml:"repo"`
-		Interval int64    `yaml:"interval"`
+		Interval int64  `yaml:"interval"`
 		Webhook  struct {
 			Enable bool   `yaml:"webhook"`
 			Secret string `yaml:"secret"`
 		} `yaml:"webhook"`
 	} `yaml:"git"`
+}
+
+func envConfigUpdate(c *Config) {
+	vars := map[string]string{
+		"SMORE_INTERFACE":      c.Interface,
+		"SMORE_PORT":           string(c.Port),
+		"SMORE_TEMPLATE":       c.Template,
+		"SMORE_DIRS_ROOT":      c.Dirs.Root,
+		"SMORE_DIRS_BASE":      c.Dirs.Base,
+		"SMORE_DIRS_STATIC":    c.Dirs.Static,
+		"SMORE_DIRS_TEMPLATES": c.Dirs.Templates,
+		"SMORE_DIRS_CONTENT":   c.Dirs.Content,
+		"SMORE_GIT_REPO":       c.Git.Repo,
+		"SMORE_GIT_INTERVAL":   "", // emp disable the update interval, set to a number otherwise
+		"SMORE_GIT_WEBHOOK":    "", // empty disables the webhook, contains secret otherwise
+	}
+	for k, v := range vars {
+		ev, ef := os.LookupEnv(k)
+		if ef {
+			switch k {
+			case "SMORE_INTERFACE":
+				c.Interface = ev
+			case "SMORE_PORT":
+				c.Port = int(ev)
+			case "SMORE_TEMPLATE":
+				c.Template = ev
+			case "SMORE_DIRS_ROOT":
+				c.Dirs.Root = ev
+			case "SMORE_DIRS_BASE":
+				c.Dirs.Base = ev
+			case "SMORE_DIRS_STATIC":
+				c.Dirs.Static = ev
+			case "SMORE_DIRS_TEMPLATES":
+				c.Dirs.Templates = ev
+			case "SMORE_DIRS_CONTENT":
+				c.Dirs.Content = ev
+			case "SMORE_GIT_REPO":
+				c.Git.Repo = ev
+			case "SMORE_GIT_INTERVAL":
+				c.Git.Interval = int64(ev)
+			case "SMORE_GIT_WEBHOOK":
+				c.Git.Webhook.Enable = true
+				c.Git.Webhook.Secret = ev
+			}
+		} else {
+			switch k {
+			case "SMORE_GIT_INTERVAL":
+				c.Git.Interval = 0
+			case "SMORE_GIT_WEBHOOK":
+				c.Git.Webhook.Enable = false
+				c.Git.Webhook.Secret = ""
+			}
+		}
+	}
 }
 
 func ReadConfig(fn string) *Config {
@@ -62,6 +116,11 @@ func ReadConfig(fn string) *Config {
 		log.Fatal("No git repo configured")
 	}
 
+	// ok, now we're done with all that
 	log.Printf("Read config: %s", fn)
+
+	// let's see if the environment has anything to say about it
+	envConfigUpdate(&retv)
+
 	return retv
 }
